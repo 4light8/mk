0d58143be68da0767946f75e22bbdb987352ca5d
Алексей <4light8@gmail.com>
2024-10-24 23:58:19 +0300

    Мигаем цветами флага России на RGB-светодиоде

--- src/led.h
+++ src/led.h
@@ -15,6 +15,26 @@ private:
     int pin;
 }; //Класс светодиода//
 
+class RGB_LED {
+
+public:
+
+    RGB_LED(int vcc_pin, int r_pin, int g_pin, int b_pin): vccpin{vcc_pin}, rpin{r_pin}, gpin{g_pin}, bpin{b_pin} {};
+
+    void init() { pinMode(vccpin, OUTPUT); pinMode(rpin, OUTPUT); pinMode(gpin, OUTPUT); pinMode(bpin, OUTPUT); }
+
+    void redLighton() { digitalWrite(vccpin, HIGH); digitalWrite(rpin, LOW); digitalWrite(gpin, HIGH); digitalWrite(bpin, HIGH); } //Подаём напряжение//
+
+    void redLightoff() { digitalWrite(vccpin, LOW); digitalWrite(rpin, HIGH); digitalWrite(gpin, HIGH); digitalWrite(bpin, HIGH);}//Напряжение равно 0//
+
+private:
+
+    int vccpin;
+    int rpin;
+    int gpin;
+    int bpin;
+}; //Класс RGB-светодиода//
+
 void delay_s(unsigned int s) {
     delay(s * 1000); //Представляем задержку в секундах//
 }
--- src/src.ino
+++ src/src.ino
@@ -1,22 +1,33 @@
-#include "led.h"
+#include "led_templ.h"
 
-constexpr unsigned int PERIOD{8};
-constexpr unsigned int HALF_PERIOD{PERIOD / 2};
+// constexpr unsigned int PERIOD{8};
+// constexpr unsigned int HALF_PERIOD{PERIOD / 2};
 
-LED builtin_led(13);
-LED another_led(8);
+using Builtin_LED = LED<13>;
+using RGBT = RGB_LED<12, 11, 9, 10>;
 
 void setup() {
-    builtin_led.init();
+    Builtin_LED::init();
+    RGBT::init();
     Serial.begin(115200);
 }
 
 void loop() {
-    builtin_led.on();
-    another_led.on();
-    delay_s(HALF_PERIOD);
-    builtin_led.off();
-    another_led.off();
-    delay_s(HALF_PERIOD);
-    Serial.print("0");
+    Builtin_LED::on();
+    RGBT::on();
+
+    RGBT::clrRGB('w');
+    delay_s(1);
+
+    RGBT::clrRGB('b');
+    delay_s(1);
+
+    RGBT::clrRGB('r');
+    delay_s(1);
+
+    Builtin_LED::off();
+    RGBT::off();
+    delay_s(2);
+
+    Serial.print("ГОЙДА!!!");
 }
